5ccaeccb54f787dd73ad3870395253ed675596ae
EmersonVilar <9569197+EmersonVilar@users.noreply.github.com>
2021-09-27 16:42:45 -0300

    Independent date interval prediction (not multiple)

--- src/predictor_manner.py
+++ src/predictor_manner.py
@@ -23,8 +23,8 @@ def predict_for_rquest(responsed_data):
     data = initial_data_processing(responsed_data)
 
     week_size = 7
-    data_obj = data_manner.Data(week_size, complete=False)
-    data_to_predict = data_obj.build_test(data, type_norm=None)
+    data_obj = data_manner.DataConstructor(week_size, is_training=False, type_norm=None)
+    data_to_predict = data_obj.build_test(data)
 
     model_loaded = load_model('models/model_test_to_load')
     prediction = model_loaded.predict(data_to_predict.x, verbose=0)
@@ -32,16 +32,12 @@ def predict_for_rquest(responsed_data):
     return prediction.reshape(-1)
 
 def convert_output_to_json(output_of_prediction, begin, end):
-    returned_dictionaty = []
-    start_date = datetime.datetime.strptime(begin, "%Y-%m-%d")
-    end_date = datetime.datetime.strptime(end, "%Y-%m-%d")
-    date_interval = [start_date + datetime.timedelta(days=interval+7) for interval in range(0, (end_date-start_date).days+1)]
-    string_date = []
-    for date in date_interval:
-        string_date.append(date.strftime("%Y-%m-%d"))
 
-    for date, value in zip(string_date, output_of_prediction):
-        returned_dictionaty.append({"date": date, "deaths": str(value)})
+    time_interval = pd.date_range(begin, end)
+
+    returned_dictionaty = []
+    for date, value in zip(time_interval, output_of_prediction):
+        returned_dictionaty.append({"date": str(date)[:10], "deaths": str(value)})
 
     returned_json = json.dumps(str(returned_dictionaty), indent=3, separators=(",", ":"))
     return returned_json
@@ -50,14 +46,26 @@ def time_delay(begin):
     start_date = datetime.datetime.strptime(begin, "%Y-%m-%d") - datetime.timedelta(days=7)
     return start_date.strftime("%Y-%m-%d")
 
+def time_skip_for_predict(begin, end):
+    start_date = datetime.datetime.strptime(begin, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(end, "%Y-%m-%d")
+    days_num = (end_date-start_date+datetime.timedelta(days=1)).days
+    days_offset = days_num + (7 - days_num % 7)
+    end_date = end_date + datetime.timedelta(days=days_offset-days_num)
+    return end_date.strftime("%Y-%m-%d")
+
 def predict(repo, path, feature, begin, end):
- 
+    original_end = end
+    original_begin = begin
+    
+    # Always call before time_delay
+    end = time_skip_for_predict(begin, end)
     begin = time_delay(begin)
     
     requested_data = pd.read_csv(f"http://ncovid.natalnet.br/datamanager/repo/{repo}/path/{path}/feature/{feature}/begin/{begin}/end/{end}/as-csv", index_col='date')
 
     predicted_values = predict_for_rquest(requested_data)
 
-    predictied_json = convert_output_to_json(predicted_values, begin, end)
+    predictied_json = convert_output_to_json(predicted_values, original_begin, original_end)
 
     return predictied_json
